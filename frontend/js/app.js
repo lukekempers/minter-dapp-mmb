7bba4e2f8eef45896c8f85d9371b5aa7d179455a
Luke Kempers <luke.kempers@gmail.com>
2022-08-24 00:59:25 -0600

    m

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -155,7 +155,6 @@ async function checkChain() {
 }
 
 async function loadInfo() {
-  window.info = await window.contract.methods.getInfo().call();
   const publicMintActive = await contract.methods.mintingActive().call();
   const presaleMintActive = await contract.methods.presaleActive().call();
   const mainHeading = document.getElementById("mainHeading");
